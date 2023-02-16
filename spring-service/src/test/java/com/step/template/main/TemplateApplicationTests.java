b9a73c0b2c73ce81d6e8ce1e4c988e6bc76fb33a
liyd <liyudong@raiyi.com>
2023-02-16 13:07:36 +0800

    初次提交

---
+++ spring-service/src/test/java/com/step/template/main/TemplateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.step.template.main;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TemplateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
